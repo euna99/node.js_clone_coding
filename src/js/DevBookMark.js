3c491d77e6a2177afeb91e5d34b43314445f38a2
euna99 <a653bc@naver.com>
2022-10-26 17:15:42 +0900

    bookMark li 틀

---
+++ src/js/DevBookMark.js
@@ -0,0 +1,63 @@
+import '../css/page2.css';
+
+
+function DevBookMark (){
+    return(
+        <div className="bookmark">
+        <p>북마크 모아보기</p>
+        <h3 class="fun">적극 채용중인 회사</h3>
+      <div class="fun_div">
+        
+          <header>
+            <img src="../img/unnamed.png" alt="" />
+          </header>
+          <footer>
+            <h4>화해</h4>
+            <h5>11개 포지션</h5>
+          </footer>
+        
+        
+          <header>
+            <img src="../img/unnamed.png" alt="" />
+          </header>
+          <footer>
+            <h4>화해</h4>
+            <h5>11개 포지션</h5>
+          </footer>
+        
+        
+          <header>
+            <img src="../img/unnamed.png" alt="" />
+          </header>
+          <footer>
+            <h4>화해</h4>
+            <h5>11개 포지션</h5>
+          </footer>
+        
+        
+          <header>
+            <img src="../img/unnamed.png" alt="" />
+          </header>
+          <footer>
+            <h4>화해</h4>
+            <h5>11개 포지션</h5>
+          </footer>
+        
+        
+          <header>
+            <img src="../img/unnamed.png" alt="" />
+          </header>
+          <footer>
+            <h4>화해</h4>
+            <h5>11개 포지션</h5>
+          </footer>
+        
+      </div>
+        </div>
+    
+
+    );
+
+
+}
+export default DevBookMark
--- src/js/Dev_page.js
+++ src/js/Dev_page.js
@@ -1,11 +1,13 @@
 import DevFilter from './Dev_Filter';
+import DevBookMark from'./DevBookMark';
 
     function Dev_Page(){
         return( 
         <div>
             <DevFilter/>
+            <DevBookMark/>
         </div>
         )
     };
-    
+
 export default Dev_Page;
