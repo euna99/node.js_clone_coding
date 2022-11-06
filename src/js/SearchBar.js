161788ff5b13eeebe24e7109e211e6825919171f
euna99 <a653bc@naver.com>
2022-11-06 22:52:42 +0900

    searchBar닫기 구현

--- src/js/SearchBar.js
+++ src/js/SearchBar.js
@@ -5,15 +5,12 @@ function SearchBar({closeSearchBar}) {
   return(
  <div className="SearchBar_Background">
   <div className="SearchBar_Wrapper">
-
     <input
      type="search"
      placeholder="#태그, 회사, 포지션 검색"
      autocomplete="off"
      value=""
     ></input>
-   <button onClick={closeSearchBar}> x </button>
-
    <div>
     <span>추천 태그로 검색해보세요</span>
     <span>기업태그 홈 이동하기</span>
@@ -24,7 +21,12 @@ function SearchBar({closeSearchBar}) {
     <button></button>
    </div>
   </div>
+    <div onClick={closeSearchBar}>
+      
+    </div>
  </div>
+
+ 
  );
 }
 export default SearchBar;
