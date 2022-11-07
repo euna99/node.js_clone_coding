a61df134db07945308908b38c04b004d9bd2b605
euna99 <a653bc@naver.com>
2022-11-07 14:24:34 +0900

    searchBar_Css

--- src/js/SearchBar.js
+++ src/js/SearchBar.js
@@ -5,21 +5,25 @@ function SearchBar({closeSearchBar}) {
   return(
  <div className="SearchBar_Background">
   <div className="SearchBar_Wrapper">
-    <input
+    <div className="SearchBar_input_div">
+    <input className="SearchBar_input"
      type="search"
      placeholder="#태그, 회사, 포지션 검색"
     //  autocomplete="off"
     ></input>
-    <button onClick={closeSearchBar}> 닫기 버튼
-    </button>
-   <div>
-    <span>추천 태그로 검색해보세요</span>
-    <span>기업태그 홈 이동하기</span>
+    </div>
+    {/* <button onClick={closeSearchBar}> 닫기 버튼
+    </button> */}
+   <div className="SearchBar_middle">
+    <div><span>추천 태그로 검색해보세요</span></div>
+    <div><span>기업태그 홈 이동하기</span></div>
    </div>
    <div className="SearchBar_Tag">
-    <button></button>
-    <button></button>
-    <button></button>
+    <button className="Search_Tag_Btn1">#기숙사</button>
+    <button className="Search_Tag_Btn2">#연봉 2%~5%</button>
+    <button className="Search_Tag_Btn3">#퇴사율 5% 미만</button>
+    <button className="Search_Tag_Btn4">#퇴사율 5% 미만</button>
+    <button className="Search_Tag_Btn5">#퇴사율 5% 미만</button>
    </div>
 
   </div>
