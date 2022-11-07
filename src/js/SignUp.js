34e756fd3403d1f78ca93aa7749c524d4ba6a235
euna99 <a653bc@naver.com>
2022-11-07 17:22:19 +0900

    signUp

--- src/js/SignUp.js
+++ src/js/SignUp.js
@@ -3,26 +3,28 @@ import '../css/SignUp.css';
 function SignUp ({closeSignUp}){
     return(
 <div className="signUp_background">
-        <div className = "signUp_wrapper">
-            <div className="signUp_header">
-            <button onClick={closeSignUp} 
-            className="signUp_close"> 취소 </button>
-            <span>회원가입</span>
-            </div>
-            <div>
-                이메일
-            <input></input>
-            </div>
-            <div>
-                이름
-            <input></input>
-            </div>
-            <div>
-                휴대폰 번호
-            <input></input>
-            </div>
-        
+    <div className = "signUp_wrapper">
+        <div className="signUp_header">
+        <button onClick={closeSignUp} 
+        className="signUp_close"> 취소 </button>
+        <span>회원가입</span>
         </div>
+        <div className="signUp_inputValue">
+            
+            <div> 이메일</div>
+            <input placeholder='email'></input>
+    
+            <div> 이름</div>
+            <input placeholder='name'></input>
+
+            <div>휴대폰 번호</div>
+            <input></input>
+
+            <div>비밀번호</div>
+            <input></input>
+
+        </div>
+    </div>
 </div>
     );
 
